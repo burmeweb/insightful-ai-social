8d4f4ad14daaf5986a188afe83591ee9321a653a
Aung Myo Kyaw <athuyrain@gmail.com>
2025-12-31 10:32:58 +0630

    Refactor Firebase config and service exports
    Refactor Firebase service initialization and exports.

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,3 @@
-// firebase/config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -6,7 +5,6 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 import { getFunctions } from "firebase/functions";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAuk2WyU_1zNyOXgmMvJPdCg2m3efB8pDA",
   authDomain: "insightful-ai-b3hca.firebaseapp.com",
@@ -19,16 +17,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const functions = getFunctions(app);
-let analytics;
+// Initialize services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app);
+export let analytics;
 
-// Initialize analytics only in client-side
 if (typeof window !== 'undefined') {
   analytics = getAnalytics(app);
 }
 
-export { app, auth, db, storage, functions, analytics };
+export { app };
